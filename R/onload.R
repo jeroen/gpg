0fa502407bf392c4e2237aef3ed9d6d95033ec56
Jeroen Ooms <jeroenooms@gmail.com>
2016-10-05 16:39:11 +0200

    Add gpg bin dir to PATH

--- R/onload.R
+++ R/onload.R
@@ -1,4 +1,9 @@
 .onLoad <- function(pkg, lib){
+  path <- Sys.getenv("PATH")
+  gpgbin <- system.file('bin', package = 'gpg')
+  if(file.exists(gpgbin) && !grepl(gpgbin, path, fixed = TRUE)){
+    Sys.setenv(PATH = paste(path, gpgbin, sep = ":"))
+  }
   try({
     engine <- gpg_restart()
     packageStartupMessage("Found GPG version ", engine$version, " in: ", engine$gpg)
