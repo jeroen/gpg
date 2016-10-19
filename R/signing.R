fcbba02c3830f6661e72de24081307aa03151bc3
Jeroen Ooms <jeroenooms@gmail.com>
2016-10-19 17:42:25 +0200

    Raise error by default if signature verification fails.

--- R/signing.R
+++ R/signing.R
@@ -8,6 +8,8 @@
 #' @aliases gpg
 #' @useDynLib gpg R_gpgme_verify
 #' @param signature path or raw vector for the gpg signature file (contains the \code{PGP SIGNATURE} block)
+#' @param error raise an error if verification fails because you do not have the
+#' signer public key in your keyring.
 #' @examples # This requires you have the Debian master key in your keyring
 #' # See https://lists.debian.org/debian-devel-announce/2014/11/msg00017.html
 #' # gpg --keyserver pgp.mit.edu --recv 0x7638d0442b90d010
@@ -16,20 +18,25 @@
 #' download.file("http://http.us.debian.org/debian/dists/jessie/Release", msg)
 #' download.file("http://http.us.debian.org/debian/dists/jessie/Release.gpg", sig)
 #' gpg_verify(msg, sig)
-gpg_verify <- function(file, signature){
+gpg_verify <- function(file, signature, error = TRUE){
   msg <- file_or_raw(file)
   sig <- file_or_raw(signature)
   out <- .Call(R_gpgme_verify, sig, msg)
   out <- data.frame(lapply(1:5, function(i){sapply(out, `[[`, i)}), stringsAsFactors=FALSE)
   names(out) <- c("fingerprint", "timestamp", "hash", "pubkey", "success");
   out$timestamp <- structure(out$timestamp, class=c("POSIXct", "POSIXt"))
-  out
+  if(isTRUE(error) && !all(out$success)){
+    fp_failed <- out$fingerprint[!(out$success)]
+    stop("Verification failed. Pubkey not found in keyring: ", paste(fp_failed, collapse = ", "), call. = FALSE)
+  } else {
+    out
+  }
 }
 
 #' @useDynLib gpg R_gpg_sign
 #' @export
 #' @param file file-path or raw vector with data to sign or verify
-#' @param id (optional) vector with key ID's to use for signing. If not set, GPG defaults
+#' @param id (optional) vector with key ID's to use for signing. If `NULL`, GPG defaults
 #' to first private key in the keyring, or what has been configured in global options.
 #' @rdname gpg_sign
 gpg_sign <- function(file, id = NULL){
