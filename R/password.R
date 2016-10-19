37f32608b48829386cc05ff9197c192fafefa3fb
Jeroen Ooms <jeroenooms@gmail.com>
2016-10-19 18:58:24 +0200

    Try to add better pinentry widget

---
+++ R/password.R
@@ -0,0 +1,36 @@
+#' Password Entry
+#'
+#' Function to prompt the user for a password to read a protected private key.
+#' IDE can provide a custom password entry widget by setting the \code{askpass}
+#' option. If no such option is specified we default to \code{\link{readline}}.
+#'
+#' @export
+#' @param prompt the string printed when prompting the user for input.
+askpass <- function(prompt = "Enter your GPG passphrase:"){
+  if(is_unix() && has_pinentry() && is_tty()){
+    pinentry(prompt)
+  } else {
+    FUN <- getOption("askpass", readline)
+    FUN(prompt)
+  }
+}
+
+is_unix <- function(){
+  identical(.Platform$OS.type, "unix")
+}
+
+is_tty <- function(){
+  return(system2("tty", "<&2") == 0)
+}
+
+has_pinentry <- function(){
+  return(system2("pinentry", "--version", stdout = FALSE, stderr = FALSE) == 0)
+}
+
+# in POSIX, "/dev/tty" means current CTTY
+pinentry <- function(str){
+  input <- c(paste("SETPROMPT", str), "GETPIN")
+  res <- system2("pinentry", paste("-T", '/dev/tty'), input = input, stdout = TRUE)
+  pwline <- res[grepl("D ", res, fixed = TRUE)]
+  sub("D ", "", pwline, fixed = TRUE)
+}
