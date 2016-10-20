1d0212ab25cbb57c954e08b992854382d192daed
Jeroen Ooms <jeroenooms@gmail.com>
2016-10-20 16:42:15 +0200

    Support 'search' parameter in gpg_recv

--- R/keys.R
+++ R/keys.R
@@ -23,24 +23,33 @@ gpg_import <- function(file){
 #' @rdname gpg_keys
 #' @param keyserver address of http keyserver. Default searches several common
 #' servers (MIT, Ubuntu, GnuPG)
-#' @param id unique ID of the pubkey (starts with `0x`)
-gpg_recv <- function(id, keyserver = NULL){
+#' @param id unique ID of the pubkey to import (starts with `0x`). Alternatively you
+#' can specify a `search` string.
+#' @param search string with name or email address to match the key info.
+gpg_recv <- function(id, search = NULL, keyserver = NULL){
   if(is.null(keyserver))
     keyserver <- c("https://pgp.mit.edu", "https://keyserver.ubuntu.com", "http://keys.gnupg.net")
   keyserver <- sub("hkp://", "http://", keyserver, fixed = TRUE)
   keyserver <- sub("/$", "", keyserver)
-  if(!identical(substring(id, 1, 2), "0x")){
-    id <- paste0("0x", id);
+  search <- if(!length(search) && length(id)){
+    id <- paste0("0x", sub("^0x", "", id));
+    if(!grepl("^0x[0-9a-fA-F]+$", id))
+      stop("ID is not valid hexadecimal string. Use 'search' to find keys by name.", call. = FALSE)
+    id
+  } else {
+    gsub(' ', '+', search)
   }
-  data <- download_key(id, keyserver)
+  data <- download_key(search, keyserver)
   gpg_import(data)
 }
 
 #' @useDynLib gpg R_gpg_delete
 #' @export
 #' @rdname gpg_keys
-gpg_delete <- function(id, secret = TRUE){
-  .Call(R_gpg_delete, id, secret)
+gpg_delete <- function(id, secret = FALSE){
+  vapply(id, function(x){
+    .Call(R_gpg_delete, x, secret)
+  }, character(1), USE.NAMES = FALSE)
 }
 
 #' @export
@@ -53,8 +62,8 @@ gpg_export <- function(id, secret = FALSE){
 #' @export
 #' @rdname gpg_keys
 #' @param secret set to `TRUE` to list/export/delete private (secret) keys
-gpg_list_keys <- function(secret = FALSE){
-  gpg_keylist_internal(secret_only = secret, local = TRUE)
+gpg_list_keys <- function(search = "", secret = FALSE){
+  gpg_keylist_internal(name = search, secret_only = secret, local = TRUE)
 }
 
 #' @useDynLib gpg R_gpg_keylist
