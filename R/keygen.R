d295bc121166817767659b74994c1a952e927919
Jeroen Ooms <jeroenooms@gmail.com>
2016-10-20 14:44:36 +0200

    Keygen documentation

--- R/keygen.R
+++ R/keygen.R
@@ -1,9 +1,11 @@
 #' GPG key generation
 #'
-#' Use `gpg_keygen` to generate a new private-public keypair. Supported options
-#' and parameters depend on the version of GPG. See the GPG manual section on
-#' [Unattended key generation](https://www.gnupg.org/documentation/manuals/gnupg/Unattended-GPG-key-generation.html).
+#' Generates a new standard private-public keypair. This function is mostly
+#' for testing purposes. Use the `gpg --gen-key` command line utility to generate an
+#' official GPG key with custom fields and options.
 #'
+#' @references GPG manual section on
+#' [Unattended key generation](https://www.gnupg.org/documentation/manuals/gnupg/Unattended-GPG-key-generation.html).
 #' @export
 #' @rdname gpg_keygen
 #' @useDynLib gpg R_gpg_keygen R_gpg_keygen_new
@@ -20,7 +22,7 @@ gpg_keygen <- function(name, email, passphrase = NULL){
     params <- list("Key-Type" = "RSA", "Name-Real" = name, "Name-Email" = email)
     controls <- "%no-ask-passphrase"
     if(length(passphrase)){
-       params["Passphrase"] <- passphrase 
+       params["Passphrase"] <- passphrase
     } else {
       controls <- c(controls, "%no-protection")
     }
@@ -30,6 +32,6 @@ gpg_keygen <- function(name, email, passphrase = NULL){
 
 make_args_str <- function(params, controls = c()){
   str <- paste(names(params), unname(params), sep = ": ", collapse = "\n")
-  str <- paste(c(str, controls), collapse = "\n") 
+  str <- paste(c(str, controls), collapse = "\n")
   paste('<GnupgKeyParms format="internal">', str, '</GnupgKeyParms>\n', sep = "\n")
 }
