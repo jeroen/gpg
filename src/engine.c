10904c815bcac9d79e1addc38458f8dac8e0a8c0
Jeroen Ooms <jeroenooms@gmail.com>
2019-01-03 17:03:02 +0100

    Remove password entry callback

--- src/engine.c
+++ src/engine.c
@@ -94,11 +94,14 @@ SEXP R_gpg_restart(SEXP home, SEXP path, SEXP pwfun, SEXP debug) {
   bail(gpgme_new(&ctx), "context creation");
   gpgme_set_armor(ctx, 1);
 
-  // Required for GPG 2.1
+
+  /* Doesn't seem to work anymore
 #if GPGME_VERSION_NUMBER >= 0x010700
+  // Required for GPG 2.1
   bail(gpgme_set_pinentry_mode(ctx, GPGME_PINENTRY_MODE_LOOPBACK), "set pinentry to loopback");
 #endif
   gpgme_set_passphrase_cb(ctx, pwprompt, pwfun);
+  */
 
   // Get engine info
   return R_engine_info();
