540d9c4ae65e550085805e8e92d6a1114d2f2ef8
Jeroen Ooms <jeroenooms@gmail.com>
2025-04-15 16:05:45 +0200

    Update version and NEWS

--- tools/gpgwin.R
+++ tools/gpgwin.R
@@ -1,4 +1,5 @@
-# Downloads
+# Downloads 'gpg classic' for windows.
+# Sadly the GnuPG in Rtools does not seem to work.
 if(!file.exists('bin/gpg.exe')){
   download.file('https://github.com/jeroen/gpg/releases/download/windows/gnupg-w32cli-1.4.23.zip', 'wingpg.zip')
   unzip('wingpg.zip')
